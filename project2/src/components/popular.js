48db6cbec62056a48958b254b9370157c61c7b71
Hannah Reilly <reillyhannah65@gmail.com>
2019-12-18 11:50:38 -0500

    popular page adjustments

--- project2/src/components/popular.js
+++ project2/src/components/popular.js
@@ -4,18 +4,31 @@ const Popular = () => (
   <div className="Popular">
     <h1>Trending Books</h1>
     <p className="popularp">
-      <h4> The Cactus </h4> 
+      <div>
+      <h3> The Cactus </h3> 
       <img src="https://images-na.ssl-images-amazon.com/images/I/51L84KhfxPL.jpg" alt="the cactus"></img>
-      <h4> The Whisper Network</h4>
+      </div>
+      <div>
+      <h3> The Whisper Network</h3>
       <img src="https://images-na.ssl-images-amazon.com/images/I/81JfVPowVmL.jpg" alt="the whisper network"></img>
-      <h4> The Giver of Stars</h4>
+      </div>
+      <div>
+        
+      <h3> The Giver of Stars</h3>
       <img src="https://images-na.ssl-images-amazon.com/images/I/91QsEyqrHiL.jpg" alt="the giver of stars"></img>
-      <h4> Fair Play</h4>
+      </div>
+      <div>
+        <h3> Fair Play</h3>
       <img src="https://images-na.ssl-images-amazon.com/images/I/71lkkIXFE7L.jpg" alt="fair play"></img>
-      <h4> The Last House Guest</h4>
+      </div>
+      <div>
+        <h3> The Last House Guest</h3>
       <img src="https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781501165375/the-last-house-guest-9781501165375_hr.jpg" alt="the last house guest"></img>
-      <h4> The Night Tiger</h4>
+      </div>
+      <div>
+        <h3> The Night Tiger</h3>
       <img src="https://images-na.ssl-images-amazon.com/images/I/91YDsOHO6XL.jpg" alt="the night tiger"></img>
+      </div>
     </p>
   </div>
 )
