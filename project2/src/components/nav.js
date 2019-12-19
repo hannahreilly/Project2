1aca7a31efba70f7d669c1f5eac938f891792b8d
Hannah Reilly <reillyhannah65@gmail.com>
2019-12-19 18:54:22 -0500

    more changes

--- project2/src/components/nav.js
+++ project2/src/components/nav.js
@@ -5,7 +5,6 @@ function Nav() {
   return (
     <div>
     <header>
-      <img className="logo" src="https://i.imgur.com/OfqsXoa.png" alt="logo" />
     </header>
     <div className="Nav">
         <ul>
