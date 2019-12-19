259305ff249949d5bbe8a5bbc3584233c5741f59
Hannah Reilly <reillyhannah65@gmail.com>
2019-12-19 11:06:36 -0500

    css

--- project2/src/components/Book.js
+++ project2/src/components/Book.js
@@ -6,7 +6,7 @@ const Book = (props) => (
     <h4>Title <br></br></h4>
     <h4>{props.book.title}</h4>
     <h4>Author <br></br> </h4>
-     <h4>{props.book.author_name}</h4>
+    <h4>{props.book.author_name}</h4>
     <h4>Publisher <br></br> </h4>
     <h4>{props.book.publisher[0]}</h4>
     <h4>First Publish Year<br></br> </h4>
