4b1e35bc90c3e71b8cc2a9fadc507f3ad1ef9f67
Hannah Reilly <reillyhannah65@gmail.com>
2019-12-20 09:20:55 -0500

    font

--- project2/src/components/about.js
+++ project2/src/components/about.js
@@ -5,11 +5,11 @@ return (
   <div className="about" >
     <h5> Who we are</h5> <br></br>
     <img className="profilePic" src="https://junkee.com/wp-content/uploads/2014/06/ellenselfie.jpg" alt="celebs"></img>
-    <h5>OverBooked is the world’s largest bookseller and a leading retailer of content, digital media and educational products. The Company operates 627 OverBooked bookstores in 50 states, 
+    <h6>OverBooked is the world’s largest bookseller and a leading retailer of content, digital media and educational products. The Company operates 627 OverBooked bookstores in 50 states, 
       and one of the Web’s premier e-commerce sites, BN.com. Our Nook Digital business offers a lineup of popular NOOK® tablets and e-Readers and an expansive collection of digital reading content through the NOOK Store®. 
       OverBooked’s mission is to operate the best omni-channel specialty retail business in America, helping both our customers and booksellers reach their aspirations, while being a credit to the communities we serve.
       The Company was founded in 1971 by bookseller Leonard Riggio when he acquired the OverBooked trade name and bookstore on Fifth Avenue in New York City. Mr. Riggio was CEO and then Executive Chairman.
-      In August of 2019, OverBooked was acquired by Elliott Advisors (UK) Limited (“Elliott”), and James Daunt, Managing Director of Waterstones bookstores in the U.K., was appointed CEO.</h5>
+      In August of 2019, OverBooked was acquired by Elliott Advisors (UK) Limited (“Elliott”), and James Daunt, Managing Director of Waterstones bookstores in the U.K., was appointed CEO.</h6>
   </div>
 )
 }
